cf8b2000e09dc0bff98187b79c4e0ce4ff733d26
yisus-c <guevarayisus1@gmail.com>
2021-11-26 13:50:52 -0600

    26/11/2021-Inicializacion

---
+++ src/yo/Ejemplo.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package yo;
+
+import java.io.IOException;
+
+/**
+ * @author gueva
+ *
+ */
+public class Ejemplo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
